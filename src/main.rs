426c9c412aee53d5e8d17156cd7f3779687f7736
Rishikesh Ramjith <kesh.ramjith@protonmail.com>
2024-09-23 18:41:50 +0200

    empty json object validation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
-    println!("Hello, world!");
+    let input_json_string = "{}";
+
+    println!("{}", validate_json_string(input_json_string.to_string()))
+}
+
+fn validate_json_string(input: String) -> bool {
+    let mut stck = Vec::new();
+
+    let mut is_valid_json = false;
+
+    for c in input.chars() {
+        match c {
+            '{' => stck.push(c),
+            '}' => {
+                let last_value = stck.pop();
+                match last_value {
+                    None => {}
+                    Some(x) => {
+                        if x == '{' {
+                            is_valid_json = true
+                        }
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+
+    is_valid_json
 }
